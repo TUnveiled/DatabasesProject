e45cda834d9cb4f2b2132ce0e67305574297f027
Negative-Feedback <matthew.giorno@sympatico.ca>
2019-03-16 14:07:32 -0400

    Implemented Decision Trees and fixed and error in apriori

--- apriori.py
+++ apriori.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from mlxtend.frequent_patterns import apriori
 
-df = pd.read_csv("C:/Users/Matthew/Downloads/dataverse_files/Electricity_P_Binary.csv")
+df = pd.read_csv("Electricity_P_Thinned_Hourly_11Norm.csv")
 df = df.loc[:, 'CWE':'TVE']
-print(df)
 results = apriori(df, min_support=0.6, use_colnames=True)
 
 print(results)
