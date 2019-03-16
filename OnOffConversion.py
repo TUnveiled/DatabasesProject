e093a5572e62586f8fe21cc959485f9f36c955ff
Negative-Feedback <matthew.giorno@sympatico.ca>
2019-03-16 13:15:40 -0400

    Fixed error in the OnOffConversion scrip

--- OnOffConversion.py
+++ OnOffConversion.py
@@ -6,12 +6,12 @@ df = df[['UNIX_TS','CWE','DWE','FRE','HPE','WOE','CDE','EBE','FGE','HTE','TVE']]
 
 thresholds = [0, 0, 68, 100, 0, 0, 0, 35, 0, 116]
 
-'''for row in range(len(df.index)):
+for row in range(len(df.index)):
     for col in range(1, 11):
         if df.iat[row, col] > thresholds[col - 1]:
             df.iat[row, col] = 1
 
         else:
-            df[row, col] = 0
+            df.iat[row, col] = 0
 
-df.to_csv("Electricity_P_Binary.csv")'''
+df.to_csv("Electricity_P_Binary.csv")
