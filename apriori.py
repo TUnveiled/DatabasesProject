688fed0f89ba6aa1f01857c007ca75dad1863a5f
Negative-Feedback <matthew.giorno@sympatico.ca>
2019-03-23 17:55:22 -0400

    Edits to kmeans. Created KMeans.csv

--- apriori.py
+++ apriori.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from mlxtend.frequent_patterns import apriori
 
-df = pd.read_csv("Electricity_P_Thinned_Hourly_11Norm.csv")
+df = pd.read_csv("Electricity_P_Thinned_Hourly_MinmaxNorm.csv")
+
 df = df.loc[:, 'CWE':'TVE']
-results = apriori(df, min_support=0.6, use_colnames=True)
+
+results = apriori(df, min_support=0.5, use_colnames=True)
 
 print(results)
--- kMeans.py
+++ kMeans.py
@@ -8,18 +8,18 @@ import matplotlib.pyplot as plt
 
 
 df = pd.read_csv("Electricity_P_Thinned_Hourly_MinmaxNorm.csv")
-df = df.loc[:, 'CWE':]
+df = df.loc[:, 'CWE':'Hour']
 #Make a copy of DF
 df_tr = df
 
 #Standardize
-clmns = ['CWE','DWE','FRE','HPE','WOE','CDE','EBE','FGE','HTE','TVE','Hour','Weekday','Month','Season']
+clmns = ['CWE','DWE','FRE','HPE','WOE','CDE','EBE','FGE','HTE','TVE','Hour']
 
 #df_tr_std = stats.zscore(df_tr[clmns])
 
 
 '''Sum_of_squared_distances = []
-K = range(1,14)
+K = range(1,48)
 for k in K:
     km = KMeans(n_clusters=k)
     km = km.fit(df_tr)
@@ -34,7 +34,8 @@ plt.show()'''
 
 
 #Cluster the data
-kmeans = KMeans(n_clusters=7, random_state=0).fit(df_tr)
+nclusters = 24
+kmeans = KMeans(n_clusters=nclusters, random_state=0).fit(df_tr)
 labels = kmeans.labels_
 
 #Glue back to originaal data
@@ -43,7 +44,7 @@ df_tr['clusters'] = labels
 #Add the column into our list
 clmns.extend(['clusters'])
 
-df_tr.sort_values(['clusters']).to_csv("KMeans.csv")
+df_tr.sort_values(['clusters']).to_csv("KMeans "+ str(nclusters) +".csv")
 
 #Lets analyze the clusters
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
